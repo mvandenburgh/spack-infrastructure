5ea0654c68c417e6ca2664cf5bbd7e26d7da210d
Mike VanDenburgh <michael.vandenburgh@kitware.com>
2024-08-21 13:57:24 -0400

    Add EKS cluster

---
+++ terraform/new/modules/aws/eks.tf
@@ -0,0 +1,58 @@
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "20.24.0"
+
+  cluster_name    = var.deployment_name
+  cluster_version = "1.30"
+
+  # Give the Terraform identity admin access to the cluster
+  # which will allow it to deploy resources into the cluster
+  enable_cluster_creator_admin_permissions = true
+  cluster_endpoint_public_access           = true
+
+  cluster_addons = {
+    coredns = {
+      addon_version = "v1.11.1-eksbuild.11"
+    }
+    eks-pod-identity-agent = {
+      addon_version = "v1.3.0-eksbuild.1"
+    }
+    kube-proxy = {
+      addon_version = "v1.30.3-eksbuild.2"
+    }
+    vpc-cni = {
+      addon_version = "v1.18.3-eksbuild.2"
+    }
+  }
+
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
+
+  eks_managed_node_groups = {
+    bootstrap-node-group = {
+      instance_types = ["m5.large"]
+
+      min_size     = 2
+      max_size     = 3
+      desired_size = 2
+
+      labels = {
+        # Used to ensure Karpenter runs on nodes that it does not manage
+        "karpenter.sh/controller" = "true"
+      }
+
+      taints = {
+        # The pods that do not tolerate this taint should run on nodes
+        # created by Karpenter
+        critical-addons-only = {
+          key    = "CriticalAddonsOnly"
+          effect = "NO_SCHEDULE"
+        }
+      }
+    }
+  }
+
+  node_security_group_tags = {
+    "karpenter.sh/discovery" = var.deployment_name
+  }
+}
