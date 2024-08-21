54cfcf121578d6a2e4528385357b707ded0f42c2
Mike VanDenburgh <michael.vandenburgh@kitware.com>
2024-08-21 14:41:05 -0400

    Add `k8s` module w/ `helm` provider

---
+++ terraform/new/modules/k8s/variables.tf
@@ -0,0 +1,14 @@
+variable "eks_cluster_endpoint" {
+  description = "The endpoint for the EKS cluster"
+  type        = string
+}
+
+variable "eks_cluster_certificate_authority_data" {
+  description = "The base64 encoded certificate authority data for the EKS cluster"
+  type        = string
+}
+
+variable "eks_cluster_name" {
+  description = "The name of the EKS cluster"
+  type        = string
+}
