e163bb9cf61959c6ec4285a1e0e1c3674e63d3f3
Mike VanDenburgh <michael.vandenburgh@kitware.com>
2024-09-04 19:09:37 -0400

    Add postgres RDS instance for gitlab

--- terraform/modules/aws/variables.tf
+++ terraform/modules/aws/variables.tf
@@ -5,3 +5,8 @@ variable "deployment_name" {
 variable "region" {
   type = string
 }
+
+variable "gitlab_db_instance_class" {
+  description = "AWS RDS DB instance class for the Spack GitLab PostgreSQL database."
+  type        = string
+}
