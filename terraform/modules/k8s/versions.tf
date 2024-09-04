f839a25fc10c4273ed51f1f603f68703ee302c65
Mike VanDenburgh <michael.vandenburgh@kitware.com>
2024-09-04 19:58:02 -0400

    Add k8s module

---
+++ terraform/modules/k8s/versions.tf
@@ -0,0 +1,46 @@
+terraform {
+  required_version = "~> 1.9.5"
+
+  required_providers {
+    kubectl = {
+      source  = "alekc/kubectl"
+      version = "~> 2.0"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2.15.0"
+    }
+    flux = {
+      source  = "fluxcd/flux"
+      version = "1.3.0"
+    }
+  }
+}
+
+data "aws_region" "current" {}
+
+provider "kubectl" {
+  host                   = var.eks_cluster_endpoint
+  cluster_ca_certificate = base64decode(var.eks_cluster_certificate_authority_data)
+
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    command     = "aws"
+    # This requires the awscli to be installed locally where Terraform is executed
+    args = ["eks", "get-token", "--cluster-name", var.eks_cluster_name]
+  }
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = var.eks_cluster_endpoint
+    cluster_ca_certificate = base64decode(var.eks_cluster_certificate_authority_data)
+
+    exec {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      command     = "aws"
+      # This requires the awscli to be installed locally where Terraform is executed
+      args = ["eks", "get-token", "--cluster-name", var.eks_cluster_name]
+    }
+  }
+}
