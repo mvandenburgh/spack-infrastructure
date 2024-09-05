f4511d1a21d26a5a79bc12b286745b4b0fb04161
Mike VanDenburgh <michael.vandenburgh@kitware.com>
2024-09-05 13:04:52 -0400

    Add analytics/cdash RDS DBs

--- terraform/modules/aws/variables.tf
+++ terraform/modules/aws/variables.tf
@@ -15,3 +15,8 @@ variable "gitlab_redis_instance_class" {
   description = "AWS ElastiCache instance class for the Spack GitLab redis instance."
   type        = string
 }
+
+variable "cdash_db_instance_class" {
+  description = "AWS RDS DB instance class for the Spack CDash MySQL database."
+  type        = string
+}
